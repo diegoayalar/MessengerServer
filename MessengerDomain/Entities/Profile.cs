c9e3b95d0667b017e8c72776da655e0cf4816a04
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-09-29 14:05:11 -0700

    Initial commit

---
+++ MessengerDomain/Entities/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace MessengerDomain.Entities
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public required int Status { get; set; }
+        public int ProfilePicURL { get; set; }
+    }
+}
---
+++ MessengerDomain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MessengerDomain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string EmailAddress { get; set; }
+        public required string Password { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required Profile Profile { get; set; }
+    }
+}
