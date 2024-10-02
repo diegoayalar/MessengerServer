a579678a8b76005bb841dbd2c7469219bc94532b
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-10-01 19:23:34 -0700

    Create AuthController.cs

---
+++ MessengerServer/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using MessengerService.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+    }
+}
