8bbb876f89114960536ae307bcea507f18f9bfa7
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-10-02 20:37:36 -0700

    Add RegisterUser functionality and validations

--- MessengerService/Service/AuthService.cs
+++ MessengerService/Service/AuthService.cs
@@ -1,7 +1,44 @@
-﻿namespace MessengerService.Service
+﻿using MessengerDomain.DTOs;
+using MessengerService.Mapper;
+using MessengerService.Validator;
+
+namespace MessengerService.Service
 {
     public class AuthService
     {
+        private readonly UserService _userService;
 
+        public AuthService(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<string> RegisterUser(NewUserDTO newUser)
+        {
+            var validation = AuthValidator.ValidateNewUser(newUser);
+            if (!validation.IsValid)
+            {
+                return validation.Message;
+            }
+
+            var existingUser = await _userService.GetUserByEmailAsync(newUser.Email);
+            if (existingUser != null)
+            {
+                return $"A user with email {existingUser.Email} already exists.";
+            }
+
+            var hashedPassword = HashPassword(newUser.Password);
+            newUser.Password = hashedPassword;
+
+            var user = UserMapper.NewUserToUser(newUser);
+            await _userService.InsertUserAsync(user);
+
+            return "User registered successfully.";
+        }
+
+        private static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
     }
 }
