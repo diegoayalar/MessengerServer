ac2b820f6a0903f40ab13c85103db79166e91a46
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-10-03 20:33:49 -0700

    Fix get query

--- MessengerService/Service/UserService.cs
+++ MessengerService/Service/UserService.cs
@@ -15,7 +15,7 @@ namespace MessengerService.Service
         public async Task InsertUserAsync(User user) => await _userRepository.InsertAsync(user);
         public async Task<IEnumerable<User>> GetAllUsersAsync() => await _userRepository.GetAllAsync();
         public async Task<User> GetUserByIdAsync(string id) => await _userRepository.GetByIdAsync(id);
-        public async Task<User> GetUserByEmailAsync(string email) => await _userRepository.GetByFieldAsync("email", email);
+        public async Task<User> GetUserByEmailAsync(string email) => await _userRepository.GetByFieldAsync("Email", email);
         public async Task UpdateUserAsync(User user) => await _userRepository.UpdateAsync(user);
         public async Task DeleteUserAsync(string id) => await _userRepository.DeleteAsync(id);
     }
