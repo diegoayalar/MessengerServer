cd183f3898af04bcf13b131968aa2c42ae0875f8
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-10-10 20:34:59 -0700

    Delete user data functions

--- MessengerService/Service/UserService.cs
+++ MessengerService/Service/UserService.cs
@@ -1,4 +1,5 @@
-﻿using MessengerDomain.Entities;
+﻿using Firebase.Database;
+using MessengerDomain.Entities;
 using MessengerPersistency.IRepository;
 
 namespace MessengerService.Service
@@ -12,11 +13,44 @@ namespace MessengerService.Service
             _userRepository = userRepository;
         }
 
-        public async Task InsertUserAsync(User user) => await _userRepository.InsertAsync(user);
+        public async Task<FirebaseObject<User>> InsertUserAsync(User user)
+        {
+            return await _userRepository.InsertAsync(user);
+        }
+
+        public async Task UpdateUserAsync(User user) => await _userRepository.UpdateAsync(user);
         public async Task<IEnumerable<User>> GetAllUsersAsync() => await _userRepository.GetAllAsync();
         public async Task<User> GetUserByIdAsync(string id) => await _userRepository.GetByIdAsync(id);
         public async Task<User> GetUserByEmailAsync(string email) => await _userRepository.GetByFieldAsync("Email", email);
-        public async Task UpdateUserAsync(User user) => await _userRepository.UpdateAsync(user);
+        public async Task UpdateUserIsActiveAsync(string id, bool isActive)
+        {
+            var user = await GetUserByIdAsync(id) ?? throw new Exception("User not found.");
+            user.IsActive = isActive;
+
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteUserDataAsync(User user)
+        {
+            DeleteCredentials(user);
+
+            if (user.Profile != null)
+            {
+                user.Profile.Description = null;
+                user.Profile.ProfilePic = null;
+                user.Profile.Status = null;
+            }
+
+            await UpdateUserAsync(user);
+        }
+
+        private void DeleteCredentials(User user)
+        {
+            user.Email = null;
+            user.Password = null;
+            user.Phone = null;
+        }
+
         public async Task DeleteUserAsync(string email) => await _userRepository.DeleteByFieldAsync("Email", email);
     }
 }
