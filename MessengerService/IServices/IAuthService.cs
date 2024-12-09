0958dcca4fb05aa2f69aebda8e648040b0d5cb7f
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-12-09 15:01:27 -0700

    Add interfaces for services

---
+++ MessengerService/IServices/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using MessengerService.DTO;
+
+namespace MessengerService.IServices
+{
+    public interface IAuthService
+    {
+        Task<(bool Success, string? Token)> RegisterUserAsync(NewUserDTO newUser);
+        Task<(bool Success, string? Token)> LoginAsync(LoginUserDTO loginUser);
+        void SignOutUser();
+        Task<(bool IsValid, string UserId)> ValidateTokenAsync(string token);
+        Task<(bool Success, string? Message)> DeleteAccountAsync(LoginUserDTO userToDelete);
+        Task SendPasswordResetEmailAsync(string email);
+    }
+}
