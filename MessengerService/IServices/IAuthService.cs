b46fa18fdc75840cdd853d1c41da3f18b6d7119a
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2025-03-06 21:51:51 -0700

    Add token related methods

--- MessengerService/IServices/IAuthService.cs
+++ MessengerService/IServices/IAuthService.cs
@@ -1,12 +1,17 @@
-﻿using MessengerService.DTO;
+﻿using Firebase.Database;
+using MessengerDomain.Entities;
+using MessengerService.DTO;
 
 namespace MessengerService.IServices
 {
     public interface IAuthService
     {
-        Task<(bool Success, string? Token)> RegisterUserAsync(NewUserDTO newUser);
+        Task<(bool Success, string? Message)> RegisterUserAsync(NewUserDTO newUser);
         Task<(bool Success, string? Token)> LoginAsync(LoginUserDTO loginUser);
         void SignOutUser();
+        Task<(bool Success, string? Message)> RefreshTokenAsync(string refreshToken);
+        Task<(bool Success, string? Message)> SaveRefreshTokenAsync(string email, RefreshToken refreshToken);
+        Task<(bool Success, string Message)> DeleteRefreshTokenAsync(string refreshToken);
         Task<(bool IsValid, string? Message)> ValidateTokenAsync(string token);
         Task<(bool Success, string? Message)> DeleteAccountAsync(LoginUserDTO userToDelete);
         Task SendPasswordResetEmailAsync(string email);
