70a09ed7ef40c69c491bf9f3276a41eee35d6ed2
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2025-03-06 21:38:37 -0700

    Add RefreshToken to User

--- MessengerDomain/Entities/User.cs
+++ MessengerDomain/Entities/User.cs
@@ -4,17 +4,12 @@
     {
         public string? Id { get; set; }
         public string? Email { get; set; }
-
         public string? Password { get; set; }
-
         public ICollection<string>? Chats { get; set; }
-
         public string? Phone { get; set; }
-
         public DateTime? DateCreated { get; set; }
-
         public bool IsActive { get; set; } = true;
-
+        public RefreshToken? RefreshToken { get; set; }
         public Profile? Profile { get; set; }
 
     }
